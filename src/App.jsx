7be5cdfba22abcf2f2edbc2ab49592888fb5dc4c
Aditya-Kumar2710 <adityaful07@gmail.com>
2026-03-09 00:40:30 +0530

    added feature to store tasks in local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import Board from "./Components/Boards/Board";
 import AddTask from "./Components/AddTask/AddTask";
 function App() {
   const [showModal , setShowModal] = useState(false);
-  const [tasks,setTasks] = useState([]);
+
+  const [tasks,setTasks] = useState(() => {
+    const savedTasks = localStorage.getItem("kanbanTasks");
+    return savedTasks ? JSON.parse(savedTasks) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("kanbanTasks", JSON.stringify(tasks));
+  }, [tasks]);
+
   return (
    <>
    <Navbar setShowModal={setShowModal}/>
